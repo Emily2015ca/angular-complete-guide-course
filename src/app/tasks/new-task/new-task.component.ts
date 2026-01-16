4e80c798c8a76def59607717221eee8d2f78bfb1
Emily <emily2015ca@gmail.com>
2026-01-15 18:59:59 -0800

    Angular Essentials- working with Modules

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,12 +1,11 @@
 import { Component, Output, EventEmitter, inject, Input } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { NewTaskData } from '../task/task.model';
+// import { FormsModule } from '@angular/forms';
 import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
-  standalone: true,
-  imports: [FormsModule],
+  standalone: false,
+  // imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
